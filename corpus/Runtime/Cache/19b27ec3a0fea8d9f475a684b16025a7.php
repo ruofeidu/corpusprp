5c65dd43fbd72a91ed8a6fe17952680530ffea82
yepyao <yepyao@78aa8591-5a99-a9db-7065-5509f5deb24c>
2011-07-03 06:15:13 +0000

    ignore Runtime

--- corpus/Runtime/Cache/19b27ec3a0fea8d9f475a684b16025a7.php
+++
@@ -1,3 +0,0 @@
-<?php if (!defined('THINK_PATH')) exit();?><!-- layout::Public:header::0 -->
-<!-- layout::$content::0 -->
-<!-- layout::Public:footer::0 -->
--- corpus/Runtime/Cache/9df3415e56c4f7c03e142fc5bda1ed34.php
+++
@@ -1,7 +0,0 @@
-<?php if (!defined('THINK_PATH')) exit();?>    <div class="row t-l">
-        <div class="b-info">
-            <div class="b-info-logo">Corpus</div>
-            <div class="b-info-copyright">Powered By SJTU Corpus PRP Group COPYRIGHT@2011</div>
-        </div>
-    </div>
-</body>
