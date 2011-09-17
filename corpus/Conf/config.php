a4100aeced884d422b96707edc57368e83811c1a
xiaoxinghai@gmail.com <xiaoxinghai@gmail.com@78aa8591-5a99-a9db-7065-5509f5deb24c>
2011-09-17 03:48:28 +0000

    增加文章内容关键字搜索功能，小修了一下文章字体颜色，增加网站图标。

--- corpus/Conf/config.php
+++ corpus/Conf/config.php
@@ -1,15 +1,14 @@
 <?php
 return array(
 	//'配置项'=>'配置值'
-	'APP_DEBUG' => true, // 开启调试模式 
+	'APP_DEBUG' => false, // 开启调试模式 
 	'URL_MODEL'	=> 1,
 	'DB_TYPE'=> 'mysql', // 数据库类型
-	'DB_HOST'=> 'phemia.db.7839587.hostedresource.com', // 数据库服务器地址
-	'DB_NAME'=>'phemia', // 数据库名称 
-	'DB_USER'=>'phemia', // 数据库用户名 
-	'DB_PWD'=>'ACMprp20', // 数据库密码 
+	'DB_HOST'=> '127.0.0.1', // 数据库服务器地址
+	'DB_NAME'=>'corpus', // 数据库名称 
+	'DB_USER'=>'root', // 数据库用户名 
+	'DB_PWD'=>'', // 数据库密码 
 	'DB_PORT'=>'3306', // 数据库端口  
 	'DB_PREFIX'=>'corpus_', // 数据表前缀
-
 );
 ?>
--- index.php
+++ index.php
@@ -1,10 +1,9 @@
 <?php 
-define('THINK_PATH', './ThinkPHP');
-define('__ROOT__', '/corpus');  
-define('APP_NAME', 'corpus'); 
-define('APP_PATH', './corpus');
-require(THINK_PATH."/ThinkPHP.php");
-session_start();
-App::run(); 
-
+	define('THINK_PATH', './ThinkPHP');
+	define('__ROOT__', '/corpus');  
+	define('APP_NAME', 'corpus'); 
+	define('APP_PATH', './corpus');
+	require(THINK_PATH."/ThinkPHP.php");
+	session_start();
+	App::run(); 
 ?>
