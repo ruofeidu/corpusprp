1be2f5725f480597e3f3a9ae8158e97a2240e3c9
xiaoxinghai@gmail.com <xiaoxinghai@gmail.com@78aa8591-5a99-a9db-7065-5509f5deb24c>
2011-11-19 15:23:51 +0000

    落了一个js文件没传上去。。

---
+++ trunk/corpus/Tpl/default/Public/error.js
@@ -0,0 +1,119 @@
+
+//手写javascript无压力了...
+errorBase = new Object(); 	//错误信息
+errorText = new Object(); 
+errorValue = new Object(); 	//错误代码
+
+errorBase['所有类型'] = new Array('不限'); 
+	errorText['不限'] = new Array('不限'); 
+	errorValue['不限'] = new Array('all'); 
+
+errorBase['书写错误'] = new Array('不限', '表记', '文章格式');
+	errorText['表记'] = new Array('表记'); 
+	errorValue['表记'] = new Array('Hk'); 
+	
+	errorText['文章格式'] = new Array('文章格式'); 
+	errorValue['文章格式'] = new Array('Ss'); 
+	
+errorBase['词语错误'] = new Array('不限', '动词', '补助动词', '名词', '形式名词', '代词', '复合词', '代词', '形容词', '助词', '终助词', '连体词', '感叹词', '接辞', '复合助词', '助动词', '接续词', '副词', '词语'); 
+
+	errorText['动词'] = new Array('动词', '词形', '自动词', '被动态', '使役态', '可能态', '授受动词', 'サ变动词', '他动词', '词性'); 
+	errorValue['动词'] = new Array('Ds', 'Ds1', 'Ds2', 'Ds3', 'Ds4', 'Ds5', 'Ds5', 'Ds6', 'Ds7', 'Ds8', 'Ds9'); 
+	
+	errorText['补助动词'] = new Array('补助动词', '词形', '授受关系补助动词', '时体补助动词', '其他补助动词'); 
+	errorValue['补助动词'] = new Array('Hj', 'Hj1', 'Hj2', 'Hj3'); 
+	
+	errorText['名词'] = new Array('名词', '词形', '词义', '词性'); 
+	errorValue['名词'] = new Array('Ms', 'Ms1', 'Ms2', 'Ms3'); 
+		
+	errorText['形式名词'] = new Array('形式名词', '词形', '词义'); 
+	errorValue['形式名词'] = new Array('Km'); 
+	
+	errorText['复合词'] = new Array('复合词', '词形', '词义', '时态复合动词'); 
+	errorValue['复合词'] = new Array('Fg'); 
+	
+	errorText['代词'] = new Array('代词', '词形', '词义'); 
+	errorValue['代词'] = new Array('Dm'); 
+	
+	errorText['形容词'] = new Array('形容词', '词形', '词义', '词性'); 
+	errorValue['形容词'] = new Array('Ky'); 
+	
+	errorText['助词'] = new Array('助词', '格助词', '接续助词'); 
+	errorValue['助词'] = new Array('Js'); 
+	
+	errorText['终助词'] = new Array('终助词语义和形式的使用错误'); 
+	errorValue['终助词'] = new Array('Syj'); 
+	
+	errorText['连体词'] = new Array('连体词'); 
+	errorValue['连体词'] = new Array('Rt'); 
+	
+	errorText['感叹词'] = new Array('感叹词'); 
+	errorValue['感叹词'] = new Array('Gd'); 
+	
+	errorText['接辞'] = new Array('接辞', '词形', '词义'); 
+	errorValue['接辞'] = new Array('Sj'); 
+	
+	errorText['复合助词'] = new Array('复合助词', '词形', '词义'); 
+	errorValue['复合助词'] = new Array('Fk'); 
+	
+	errorText['助动词'] = new Array('助动词', '词形', '词义'); 
+	errorValue['助动词'] = new Array('Jd'); 
+	
+	errorText['接续词'] = new Array('接续词', '词形', '词义'); 
+	errorValue['接续词'] = new Array('St'); 
+	
+	errorText['副词'] = new Array('副词', '词形', '词义', '词性'); 
+	errorValue['副词'] = new Array('F'); 
+	
+errorBase['词组错误'] = new Array('不限', '动/动搭配', '副/动搭配', '名/动搭配', '名/名搭配', '形/名搭配', '形动/名搭配', '形动/动搭配', '形/动搭配', '固定词组', '名/形搭配'); 
+errorValue['词组错误'] = 'R';
+
+errorBase['词法错误'] = new Array('不限', '重复', '欠缺', '语体', '语序', '时态'); 
+errorValue['词法错误'] = 'W';
+
+errorBase['句子错误'] = new Array('不限', '重复、多余', '欠缺', '时态', '标点', '语序', '句子置换', '文体', '肯定与否定'); 
+errorValue['句子错误'] = 'W';
+
+function set_city(province, city, error)
+{
+   //改变二级级联下拉菜单的选项供选择
+	var pv, i;
+
+	pv = province.value;
+	city.length = 1;
+
+	if (pv == '0') return;
+	if (typeof(errorBase[pv]) == 'undefined') return;
+
+	for (i = 0; i < errorBase[pv].length; i++)
+	{
+		city.options[i] = new Option();
+		city.options[i].text = errorBase[pv][i];
+		city.options[i].value = errorBase[pv][i];
+	}
+    set_next(city, error); 
+}
+
+function set_next(city, error)
+{
+   //改变二级级联下拉菜单的选项供选择
+	var cv, i;
+
+	cv = city.value;
+	error.length = 1;
+
+	if (cv == '0') return;
+	if (typeof(errorText[cv]) == 'undefined') {
+		error.options[0] = new Option();
+		error.options[0].text = cv;
+		error.options[0].value = errorValue[cv] + city.index;
+		return;
+	}
+	
+	for (i = 0; i < errorText[cv].length; i++)
+	{
+		error.options[i] = new Option();
+		error.options[i].text = errorText[cv][i];
+		error.options[i].value = errorValue[cv] + i;
+	}
+}
