267a9a4cef2f79c5b39cc7153ef272583a810d25
xiaoxinghai@gmail.com <xiaoxinghai@gmail.com@78aa8591-5a99-a9db-7065-5509f5deb24c>
2011-12-28 02:58:27 +0000

    统一config.php，使用同一个数据库。

--- trunk/corpus/Conf/config.php
+++ trunk/corpus/Conf/config.php
@@ -4,10 +4,10 @@ return array(
 	'APP_DEBUG'		=>	true, 			// 开启调试模式 
 	'URL_MODEL'		=>	1,
 	'DB_TYPE'		=>	'mysql', 		// 数据库类型
-	'DB_HOST'		=>	'127.0.0.1', 	// 数据库服务器地址
+	'DB_HOST'		=>	'202.120.38.239', 	// 数据库服务器地址
 	'DB_NAME'		=>	'corpus', 		// 数据库名称 
-	'DB_USER'		=>	'root', 		// 数据库用户名 
-	'DB_PWD'		=>	'', 			// 数据库密码 
+	'DB_USER'		=>	'corpus', 		// 数据库用户名 
+	'DB_PWD'		=>	'ACMclass',		// 数据库密码 
 	'DB_PORT'		=>	'3306', 		// 数据库端口  
 	'DB_PREFIX'		=>	'corpus_', 		// 数据表前缀
 	
