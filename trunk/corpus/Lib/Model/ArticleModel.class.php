82dc0d9f8b5613f2b59986586d19ecce09d6d584
xiaoxinghai@gmail.com <xiaoxinghai@gmail.com@78aa8591-5a99-a9db-7065-5509f5deb24c>
2012-03-11 03:09:36 +0000

    修复了编辑文章和添加文章的BUG。BUG原因：编辑文章更换模板的时候少了hidden id，添加文章数据库的id不是auto_increase的！

--- trunk/corpus/Lib/Model/ArticleModel.class.php
+++ trunk/corpus/Lib/Model/ArticleModel.class.php
@@ -2,19 +2,20 @@
 class ArticleModel extends CommonModel {
 	// 自动验证设置
 	protected $_validate	 =	 array(
-		array('aid','require','文章id要填写哦~',1),
-		array('semester','require','学期要填写哦~',1),
+		//array('aid','require','请填写作品コード',1),
+		//array('semester','require','请填写学期编号',1),
 	
-		array('title','require','标题要填写哦~',1),
-		array('text','require','文章内容要填写哦~'),
-		
-		array('aid','','编辑成功', 0, 'unique', self::MODEL_INSERT),
-		);
+		//array('title','require','请填写題目',1),
+		//array('text','require','请填写文章内容',1),
+	
+		//array('id','','ID已经存在', 0, 'unique', self::MODEL_INSERT)
+	);
+	
 	// 自动填充设置
 	protected $_auto	 =	 array(
-		array('status','1',self::MODEL_INSERT),
-		array('create_time','time',self::MODEL_INSERT,'function'),
-		);
+		//array('status','1',self::MODEL_INSERT),
+		//array('create_time','time',self::MODEL_INSERT,'function'),
+	);
 
 }
 ?>
