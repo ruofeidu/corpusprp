a5c55bd179bb138f3fcfc0d3d20a7bba59d75a61
xiaoxinghai@gmail.com <xiaoxinghai@gmail.com@78aa8591-5a99-a9db-7065-5509f5deb24c>
2012-03-12 04:42:58 +0000

    学生管理系统完成

--- corpus/Lib/Model/StudentModel.class.php
+++ corpus/Lib/Model/StudentModel.class.php
@@ -1,14 +1,14 @@
 <?php
-class ArticleModel extends CommonModel {
+class StudentModel extends CommonModel {
 	// 自动验证设置
 	protected $_validate	 =	 array(
-		array('aid','require','请填写作品コード',1),
-		array('semester','require','请填写学期编号',1),
+		array('uid','require','请填写作者コード',1),
+		array('name','require','请填写作者姓名',1),
 	
-		array('title','require','请填写題目',1),
+		//array('title','require','请填写題目',1),
 		//array('text','require','请填写文章内容',1),
 	
-		array('id','','ID已经存在', 0, 'unique', self::MODEL_INSERT)
+		//array('id','','ID已经存在', 0, 'unique', self::MODEL_INSERT)
 	);
 	
 	// 自动填充设置
